267ca9542be4b56cef430fba690254e1f0dfb861
turasova <134943646+turasova@users.noreply.github.com>
2023-10-04 16:09:57 +0300

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,11 @@
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-import SimpleLightbox from 'simplelightbox';
-import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 
 const galleryList = document.querySelector('.gallery');
