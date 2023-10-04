5951fadfb6a8f31a87b899b29185ddc21d12753e
turasova <134943646+turasova@users.noreply.github.com>
2023-10-04 15:49:56 +0300

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const galleryList = document.querySelector('.gallery');
+
+
+galleryList.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
+
+
+
+function createMarkup(arr) {
+    return arr.map(({preview, original, description }) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    ).join('');
+}
+console.dir(galleryList)
+
+// use library SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
