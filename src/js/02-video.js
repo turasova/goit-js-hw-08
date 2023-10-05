d9c158b90ce8a983a486c2296fbf2d2efd9d6835
turasova <134943646+turasova@users.noreply.github.com>
2023-10-05 14:11:11 +0300

    02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,41 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+    const player = new Player(iframe);
+
+
+const onPlay = function (data) {
+     // data is an object containing properties specific to that event
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+console.log(localStorage)
+ 
+
+ player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+ 
+console.log(currentTime)
+
+ player.setCurrentTime(currentTime).then(function (seconds) {
+  // seconds = the actual time that the player seeked to
+ }).catch(function (error) {
+       switch (error.name) {
+     case 'RangeError':
+        //  the time was less than 0 or greater than the video’s duration
+               break;
+           default:
+          // some other error occurred
+             break;
+    }
+ });
+
+
+
+
+
+
+
+
 
